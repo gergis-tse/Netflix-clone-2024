4ea348f77d66c5cb58944dc83cd9508831761ec6
Gergis <gergistse@gmail.com>
2024-11-13 21:32:59 -0700

    Banner

--- netflix-clone/src/pages/Home.jsx
+++ netflix-clone/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import ".././index.css"
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
-
+import Banner from "../components/Banner/Banner"
 function Home() {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
