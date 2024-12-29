30eb9a73edf28ec49016c314b990573765008a4d
Gergis <gergistse@gmail.com>
2024-12-28 23:34:12 -0700

    remove amazon folder from this repository

--- AmazonWebsite/AmazonFrontEnd/src/Components/CurrenctFormat/CurrencyFormat.jsx
+++
@@ -1,9 +0,0 @@
-import numeral from "numeral"
-const CurrencyFormat=({amount})=>{
-const formattedAmount=numeral(amount).format("$0,0.00")
-return <div>{formattedAmount}</div>
-}
-
-
-
-export default CurrencyFormat
